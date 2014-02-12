db04056176d2221fff0f9341da8e0c52857c673a
shpuntik <shpuntik74@gmail.com>
2014-02-12 21:08:14 +0600

    В тесте исправлено количество городов при проверке

--- app/tests.py
+++ app/tests.py
@@ -9,7 +9,7 @@ from weather_parsers import YandexWeatherParser, YahooWeatherParser
 class WeatherParserTests(TestCase):
 	def test_updateWeather(self):
 		"""
-		После вызова команды 'update_weather' количество записей в бд увеличится для каждого сервиса на 3
+		После вызова команды 'update_weather' количество записей в бд должно увеличиться на N для каждого сервиса, где N количество городов
 		"""
 		recordsYandexBefore = YandexWeather.objects.count()
 		recordsYahooBefore = YahooWeather.objects.count()
@@ -19,5 +19,5 @@ class WeatherParserTests(TestCase):
 		recordsYandexAfter = YandexWeather.objects.count()
 		recordsYahooAfter = YahooWeather.objects.count()
 
-		self.assertEqual(recordsYandexBefore + 3, recordsYandexAfter)
-		self.assertEqual(recordsYahooBefore + 3, recordsYahooAfter)
+		self.assertEqual(recordsYandexBefore + len(YANDEX_CITY_LIST), recordsYandexAfter)
+		self.assertEqual(recordsYahooBefore + len(YAHOO_CITY_LIST), recordsYahooAfter)
